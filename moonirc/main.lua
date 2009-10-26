240a701e3b82c20b5e01247c1218fb6019036323
Linus Sjögren <thelinx@unreliablepollution.net>
2009-10-26 15:37:27 +0100

    beginning...

---
+++ moonirc/main.lua
@@ -0,0 +1,112 @@
+require("socket")
+
+print("Starting MoonIRC Daemon... ")
+
+soc = assert(socket.tcp(), "could not create socket")
+assert(soc:bind('*', 6667), "could not bind socket")
+assert(soc:listen(), "could not initiate socket")
+assert(soc:settimeout(0), "could not set timeout")
+assert(soc:setoption('keepalive', true), "could not set keepalive")
+print("Listening for connections...")
+
+USERS = {}
+CHANNELS = {}
+SERVERS = {}
+
+function string.split(str, pat)
+-- from http://lua-users.org/wiki/SplitJoin
+   local t = {}
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+	 table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
+
+function parsecommand(command)
+    assert(type(command) == "string", "bad argument #1 to 'parsecommand' (string expected, got "..type(command)..") "..command)
+    local fromuser = ""
+    if command:sub(1,1) == ":" then
+        local x = command:find(" ")
+        fromuser = command:sub(2,x)
+        command = command:sub(x+1)
+    end
+    local arguments = ""
+    do
+        local x = command:find(" ")
+        arguments = command:sub(x+1)
+        command = command:sub(1,x-1)
+    end
+    do
+        local lastarg = ""
+        if arguments:find(":") then
+            local x = arguments:find(":")
+            lastarg = arguments:sub(x+1)
+            arguments = arguments:sub(x)
+        end
+        arguments = arguments:split(" ")
+        if lastarg then arguments[#arguments+1] = lastarg end
+    end
+    return command,arguments,fromuser
+end
+
+function irc_pass(user, arg)
+    if user.pass then
+        user.socket:send("PASS :You may not reregister")
+    elseif not arg then
+        user.socket:send("PASS :Not enough parameters")
+    else
+        user.pass = arg
+    end
+    return user
+end
+function irc_nick(user, arg)
+    if 
+
+function handshake(user)
+    print("connection initiated from "..user.ip)
+    while true do
+        local c,a = parsecommand(user.socket:receive("*l"))
+        if c == "PASS" then
+            user = irc_pass(user, a)
+        elseif c == "NICK" then
+            user = irc_nick(user, a)
+        elseif c == "USER" then
+            user = irc_user(user, a)
+            break
+        else
+            error("user is a fag ("..c..","..unpack(a)..")")
+        end
+        USERS[user.ip] = user
+    end
+    USERS[user.ip] = user
+    return true
+end
+
+while true do
+    local ret,err = soc:accept()
+    if ret then
+        local ret_ip,ret_port = ret:getpeername()
+        USERS[ret_ip] = {
+            socket = ret,
+            ip = ret_ip,
+            port = ret_port
+        }
+        handshake(USERS[ret_ip])
+    else
+        assert(err == "timeout", err)
+    end
+    for _,v in pairs(USERS) do
+        parsecommand(v.socket:receive("*l"))
+    end
+end
